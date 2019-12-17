9ed4e4fb6db4d3377cfbe225c0da1f95ba0ea4da
齋藤大輔 <saitoudaisuke@saitoudaisukenoMacBook-Air.local>
2019-12-18 01:29:38 +0900

    create root

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css'
-import App from './App'
+import Index from './pages/index'
+import Show from './pages/show'
+import New from './pages/new'
+import Edit from './pages/edit'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Router>
+    <Route path="/" exact component={Index} />
+    <Route path="/books" exact component={Index} />
+    <Route path="/books/new" exact component={New} />
+    <Route path="/books/:id/show" exact component={Show} />
+    <Route path="/books/:id/edit" exact component={Edit} />
+  </Router>,
+  document.getElementById('root')
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()

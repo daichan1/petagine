8d83c80a2e929c321d9786fb92940044ca51c4c1
齋藤大輔 <saitoudaisuke@saitoudaisukenoMacBook-Air.local>
2019-12-22 13:25:20 +0900

    create link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import { Link } from 'react-router-dom'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
@@ -13,12 +14,23 @@ const useStyles = makeStyles((theme: Theme) =>
       textAlign: 'center',
       backgroundColor: '#C0C0C0'
     },
+    toolBar: {
+      display: 'flex'
+    },
     grid: {
       marginTop: theme.spacing(3)
     },
     img: {
       width: '80%',
       height: 300
+    },
+    link: {
+      color: 'white',
+      marginLeft: 'auto',
+      textDecoration: 'none'
+    },
+    text: {
+      textAlign: 'right'
     }
   })
 )
@@ -44,14 +56,19 @@ export default function Index() {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar className={classes.toolBar}>
           <Typography variant="h6">書籍一覧</Typography>
+          <Link to="/books/new" className={classes.link}>
+            新規作成
+          </Link>
         </Toolbar>
       </AppBar>
       <Grid container spacing={3}>
         {images.map(image => (
           <Grid key={image} item xs={6} sm={3} className={classes.grid}>
-            <img src={image} alt="書籍" className={classes.img} />
+            <Link to="/books/1/show">
+              <img src={image} alt="書籍" className={classes.img} />
+            </Link>
           </Grid>
         ))}
       </Grid>
